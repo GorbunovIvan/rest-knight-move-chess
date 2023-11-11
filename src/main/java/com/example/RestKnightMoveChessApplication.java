6148dd4baf76e0bf7382cfd9e33f93dfafe793bb
GorbunovIvan <gorbunovvivan@gmail.com>
2023-11-11 13:36:40 +0200

    init

---
+++ src/main/java/com/example/RestKnightMoveChessApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestKnightMoveChessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestKnightMoveChessApplication.class, args);
+    }
+}
